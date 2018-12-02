44d75fac9eacd74066984091ccb4e11795070eb6
Incomplete <incomplete@aixon.co>
2018-12-02 14:26:11 +0800

    Add a function to start the GraphDB process

--- src/GraphDB.hs
+++ src/GraphDB.hs
@@ -1,9 +1,10 @@
 module GraphDB
-  ( loop
+  ( start
   , Poison(..)
   )
   where
 
+import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Monad
 import qualified Database.Bolt as Bolt
@@ -15,11 +16,12 @@ data Poison a
   | Payload a
 
 
-loop :: Bolt.BoltCfg -> TBQueue (Poison Text) -> IO ()
-loop boltCfg queryQueue = do
+start :: Bolt.BoltCfg -> TBQueue (Poison Text) -> IO ThreadId
+start boltCfg queryQueue = do
   pipe <- Bolt.connect boltCfg
-  go pipe queryQueue
+  forkIO $ go pipe queryQueue
   where
+    go :: Bolt.Pipe -> TBQueue (Poison Text) -> IO ()
     go pipe queue = do
       query' <- atomically $ readTBQueue queue
       case query' of
