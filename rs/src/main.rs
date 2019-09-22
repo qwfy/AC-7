8b71779f048130fb9913479fd90358dacf67cb51
Incomplete <incomplete@aixon.co>
2019-09-22 10:49:46 +0800

    Binary skeleton for the NEAT algorithm

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,24 @@
+use std::str::FromStr;
+use clap::{Arg, App, SubCommand};
+use ac7;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("AC-7")
+        .author("Incomplete <incomplete@aixon.co>")
+        .subcommand(SubCommand::with_name("neat")
+            .about("The NEAT algorithm")
+            .arg(Arg::with_name("generation")
+                .short("g")
+                .long("generation")
+                .required(true)
+                .takes_value(true)
+                .help("number of generates to simulate")))
+        .get_matches();
+    if let Some(matches) = matches.subcommand_matches("neat") {
+        let generation = matches.value_of("generation").unwrap();
+        let num_generations = i32::from_str(generation).unwrap();
+        ac7::neat::simulate(num_generations);
+    } else {
+        println!("Bad command");
+    }
 }
---
+++ rs/src/neat.rs
@@ -0,0 +1,3 @@
+pub fn simulate(num_generations: i32) {
+  println!("Number of generations to simulate: {}", num_generations);
+}
