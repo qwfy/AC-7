0458bab48030c27723def05139883b1f00796612
Incomplete <incomplete@aixon.co>
2019-09-24 22:47:37 +0800

    Logging

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,5 +1,9 @@
 use std::str::FromStr;
 use clap::{Arg, App, SubCommand};
+use fern;
+use log;
+use chrono;
+
 use ac7;
 
 fn main() {
@@ -14,6 +18,7 @@ fn main() {
                 .takes_value(true)
                 .help("number of generates to simulate")))
         .get_matches();
+    setup_logger().unwrap();
     if let Some(matches) = matches.subcommand_matches("neat") {
         let generation = matches.value_of("generation").unwrap();
         let num_generations = u32::from_str(generation).unwrap();
@@ -21,4 +26,23 @@ fn main() {
     } else {
         println!("Bad command");
     }
+}
+
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                    "{} [{}] [{}] {}",
+                    chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                    record.level(),
+                    record.target(),
+                    message
+            ))
+        })
+    .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("ac7.log")?)
+        .apply()?;
+    Ok(())
 }
